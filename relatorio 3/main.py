2c48aed0b3f7f1bf5783e5b448437a4abc639bcf
Warley Ruivo <125412705+warleyruivo@users.noreply.github.com>
2025-03-13 18:43:29 -0300

    Relatorio 3

--- relatorio 3/main.py
+++ relatorio 3/main.py
@@ -1,11 +1,30 @@
 from database import Database
-from helper.WriteAJson import writeAJson
+from helper.writeAJson import writeAJson
+from pokedex import Pokedex 
 
+# Criar a conexão com o banco de dados
 db = Database(database="pokedex", collection="pokemons")
-db.resetDatabase()
 
-pokemons_dataset = db.collection.find()
+# Instanciar a Pokedex passando o banco de dados
+pokedex = Pokedex(db)
 
-git remote add origin https://github.com/warleyruivo/s202-lab.git
-git branch -M main
-git push -u origin main
+# Executar algumas consultas
+print("Obtendo todos os Pokémons...")
+all_pokemons = pokedex.get_all_pokemons()
+print(f"Total de Pokémons encontrados: {len(all_pokemons)}")
+
+print("\nBuscando Pokémon pelo nome (Pikachu)...")
+pokemon = pokedex.get_pokemon_by_name("Pikachu")
+print(pokemon)
+
+print("\nBuscando Pokémons do tipo 'Fire'...")
+fire_pokemons = pokedex.get_pokemon_by_type("Fire")
+print(f"Total de Pokémons do tipo 'Fire': {len(fire_pokemons)}")
+
+print("\nBuscando Pokémons com ataque acima de 50...")
+strong_pokemons = pokedex.get_pokemon_by_base_stat("Attack", 50)
+print(f"Total de Pokémons com ataque >= 50: {len(strong_pokemons)}")
+
+print("\nContando quantidade de Pokémons por tipo...")
+pokemon_count = pokedex.count_pokemons_by_type()
+print(pokemon_count)
--- relatorio 3/pokedex.py
+++ relatorio 3/pokedex.py
@@ -1,181 +1,42 @@
+from database import Database
+from helper.writeAJson import writeAJson
+
 class Pokedex:
     def __init__(self, database: Database):
         self.database = database
-
-    def getPokemonByName(self, name: str):
-        return self.database.collection.find({"name": name})
-
-    def getPokemonsByType(self, types: list):
-        return self.database.collection.find({"type": {"$in": types}})
-
-    def getPokemonsWithEvolutionsByType(self, types: list):
-        return self.database.collection.find({"type": {"$in": types}, "next_evolution": {"$exists": True}})
-
-    def getPokemonsByWeaknesses(self, weaknesses: list):
-        return self.database.collection.find({"weaknesses": {"$all": weaknesses}})
-
-    def getPokemonsWithSpecificWeaknessesCount(self, size: int):
-        return self.database.collection.find({"weaknesses": {"$size": size}})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    def get_all_pokemons(self):
+        """Consulta todos os pokémons da coleção."""
+        pokemons = list(self.database.collection.find())
+        writeAJson(pokemons, "all_pokemons")
+        return pokemons
+    
+    def get_pokemon_by_name(self, name: str):
+        """Consulta um pokémon pelo nome."""
+        pokemon = self.database.collection.find_one({"name": name}) or {}
+        writeAJson(pokemon, f"pokemon_{name}")
+        return pokemon
+    
+    def get_pokemon_by_type(self, type_: str):
+        """Consulta todos os pokémons de um determinado tipo."""
+        pokemons = list(self.database.collection.find({"type": {"$in": [type_]}}))
+        writeAJson(pokemons, f"pokemons_type_{type_}")
+        return pokemons
+    
+    def get_pokemon_by_base_stat(self, stat: str, min_value: int):
+        """Consulta todos os pokémons que possuem um valor mínimo para um determinado atributo base."""
+        query = {f"base.{stat}": {"$gte": min_value}}
+        pokemons = list(self.database.collection.find(query))
+        writeAJson(pokemons, f"pokemons_{stat}_gte_{min_value}")
+        return pokemons
+    
+    def count_pokemons_by_type(self):
+        """Conta a quantidade de pokémons por tipo."""
+        pipeline = [
+            {"$unwind": "$type"},
+            {"$group": {"_id": "$type", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}}
+        ]
+        result = list(self.database.collection.aggregate(pipeline))
+        writeAJson(result, "count_pokemons_by_type")
+        return result
