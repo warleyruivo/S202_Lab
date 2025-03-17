bcc47358df1f446a3da8605691e18a2c40a71c07
Warley Ruivo <125412705+warleyruivo@users.noreply.github.com>
2025-03-17 16:38:04 -0300

    Relatorio 4

--- EA1/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class Database:
-    def __init__(self, uri="mongodb://localhost:27017/", db_name="transporte"):
-        self.client = MongoClient(uri)
-        self.db = self.client[db_name]
-
-    def get_collection(self, name):
-        return self.db[name]
--- EA1/main.py
+++
@@ -1,9 +0,0 @@
-from cli_motorista import MotoristaCLI
-
-def main():
-    cli = MotoristaCLI()
-    cli.menu()
-
-if __name__ == "__main__":
-    main()
-1
